1bbab7d5cfc391a4240f39e86a84a95e66b5f08f
Jashele T. <48870048+jasheloper@users.noreply.github.com>
2024-10-25 19:40:36 -0700

    adds files

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# promise-animations
+# Sequencing Animations
+
+To test understanding of how to use promise-based APIs.
+
 Update a page to play a series of animations in a sequence using promise-based techniques.
+
+<br>
+
+## Notes
+
+The project required to implement promise based code 3 different ways:
+
+<br>
+
+### Version 1: Callback Hell
+
+```
+function firstAlice() {
+  return alice1.animate(aliceTumbling, aliceTiming);
+}
+
+function secondAlice() {
+  return alice2.animate(aliceTumbling, aliceTiming);
+}
+
+function thirdAlice() {
+  return alice3.animate(aliceTumbling, aliceTiming);
+}
+
+
+function allAlice() {
+  firstAlice().finished.then( () => {
+    secondAlice().finished.then( () => {
+      thirdAlice().finished
+    });
+  });
+}
+
+allAlice();
+```
+
+<br>
+
+### Version 2: Promise Chain
+
+<br>
+
+### Version 3: async and await
---
+++ main.js
@@ -0,0 +1,47 @@
+const aliceTumbling = [
+{ transform: 'rotate(0) scale(1)' },
+{ transform: 'rotate(360deg) scale(0)' }
+];
+
+const aliceTiming = {
+duration: 2000,
+iterations: 1,
+fill: 'forwards'
+}
+
+const alice1 = document.querySelector("#alice1");
+const alice2 = document.querySelector("#alice2");
+const alice3 = document.querySelector("#alice3");
+
+/*
+console.log(alice1.animate(aliceTumbling, aliceTiming));
+console.log(alice1.animate(aliceTumbling, aliceTiming).finished);
+
+alice1.animate(aliceTumbling, aliceTiming).finished.then( () => {
+  console.log("finished!");
+});
+*/
+
+
+function firstAlice() {
+  return alice1.animate(aliceTumbling, aliceTiming);
+}
+
+function secondAlice() {
+  return alice2.animate(aliceTumbling, aliceTiming);
+}
+
+function thirdAlice() {
+  return alice3.animate(aliceTumbling, aliceTiming);
+}
+
+
+function allAlice() {
+  firstAlice().finished.then( () => {
+    secondAlice().finished.then( () => {
+      thirdAlice().finished
+    });
+  });
+}
+
+allAlice();
