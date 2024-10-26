f90e0ef85c9e88c35d86247197ebb5f810e5b5a9
Jashele T. <48870048+jasheloper@users.noreply.github.com>
2024-10-26 11:54:38 -0700

    update: Arrow function with implicit return

--- main.js
+++ main.js
@@ -26,6 +26,44 @@ function thirdAlice() {
   return alice3.animate(aliceTumbling, aliceTiming);
 }
 
+/*
+For exploring different arrow function forms, you could try variations like using implicit returns, omitting parentheses for single parameters, or using traditional function syntax. Each variation would still maintain the promise chain structure.
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+*/
+
+
+/*
+Arrow function with implicit returns - curly braces and 'return' keyword are omitted.
+
+Note: The parentheses can only be omitted if the function has a single simple parameter. If it has multiple parameters, no parameters, or default, destructured, or rest parameters, the parentheses around the parameter list are required.
+  */
+ 
+firstAlice().finished 
+  .then( () => secondAlice().finished)
+  .then( () => thirdAlice().finished);
+
+
+
+/*
+Traditional function syntax instead of arrow functions, with explicit return statements.
+
+firstAlice().finished
+  .then( function () {
+    return secondAlice().finished
+  })
+  .then ( function () {
+    return thirdAlice().finished
+  })
+*/
+
+
+
+
+
+/*
+Arrow function with explicit return statmement and curly braces for the function body.
+
 firstAlice().finished
   .then( () => {
     return secondAlice().finished;
@@ -33,8 +71,14 @@ firstAlice().finished
   .then( () => {
     return thirdAlice().finished;
   })
+    */
+  
+    
+
 
 /*
+callback hell 
+
 function allAlice() {
   firstAlice().finished.then( () => {
     secondAlice().finished.then( () => {
